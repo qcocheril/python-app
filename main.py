ce3cdd7f462cf4c9bccec068376a4717b9d58b9d
qcocheril <quentin.cocheril@gmail.com>
2023-09-19 12:31:19 +0200

    third commit

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     return "Hello from World! 🚀"
+def hello_world():
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=YOUR_GA_CODE"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', 'G-DVK1THXJ2C');
+</script>
+ """
+ return prefix_google + "Hello World"
