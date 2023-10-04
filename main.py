00c49277eb9a32acd89b6d6b0624f2f350e8fe48
qcocheril <quentin.cocheril@gmail.com>
2023-10-04 11:22:46 +0200

    Lab2 first commit

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-
-def hello_world():
- prefix_google = """
- <!-- Google tag (gtag.js) -->
+prefix_google = """
+<!-- Google tag (gtag.js) -->
 <script async
 src="https://www.googletagmanager.com/gtag/js?id=G-DVK1THXJ2C"></script>
 <script>
@@ -15,5 +12,24 @@ src="https://www.googletagmanager.com/gtag/js?id=G-DVK1THXJ2C"></script>
  gtag('js', new Date());
  gtag('config', 'G-DVK1THXJ2C');
 </script>
- """
+"""
+
+@app.route("/")
+
+def hello_world():
  return prefix_google + "Hello World"
+
+@app.route("/logger")
+
+def logger():
+
+ log_msg = "Logger route accessed"
+ browser_log = """
+ <script>
+    console.log("This is a log message");
+ </script>
+ """
+ print(log_msg)
+ return log_msg + browser_log
+
+
